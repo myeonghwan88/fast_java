8163a857ddc5036f7919710bb11628afb873b98e
조명환 <idmash88@gmail.com>
2023-05-28 21:42:21 +0900

    정수자료형

---
+++ Chapter01/src/ch06/VariableTest.java
@@ -0,0 +1,16 @@
+package ch06;
+
+public class VariableTest {
+
+	public static void main(String[] args) {
+		
+		byte bnum = 127;
+		
+		System.out.println(bnum);
+
+		
+//		int num = 123456789000;
+		long lNum = 123456789000L;
+		System.out.println(lNum);
+	}
+}
