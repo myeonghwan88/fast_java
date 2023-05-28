4afe381e1563e6f280d63d393f8af2689f3373d2
조명환 <idmash88@gmail.com>
2023-05-28 22:52:42 +0900

    형변환 강제캐스팅

---
+++ Chapter01/src/ch10/TypeConversion.java
@@ -0,0 +1,30 @@
+package ch10;
+
+public class TypeConversion {
+
+	public static void main(String[] args) {
+
+//		byte bnum = 125;
+//		int inum = bnum;
+//		System.out.println(inum);
+		
+//		int inum = 255;
+//		byte bNum = (byte)inum;		
+//
+//		System.out.println(bNum);
+		
+//		double dNum = 3.14;
+//		int inum = (int)dNum;
+//		
+//		System.out.println(inum);
+		
+		double dNum = 1.2;
+		float fNum = 0.9f;
+		
+		int iNum1 = (int)dNum + (int)fNum;
+		int iNum2 = (int)(dNum + fNum);
+		
+		System.out.println(iNum1);
+		System.out.println(iNum2);
+	}
+}
