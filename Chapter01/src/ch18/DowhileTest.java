8ce52a947a81446395c1088424f43abfb4159321
조명환 <idmash88@gmail.com>
2023-05-30 02:37:28 +0900

    do-while

---
+++ Chapter01/src/ch18/DowhileTest.java
@@ -0,0 +1,28 @@
+package ch18;
+
+import java.util.Scanner;
+
+public class DowhileTest {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		int input;
+		int sum = 0;
+		
+		System.out.print("값 입력:");
+//		input = scanner.nextInt();
+		
+//		while( input != 0 ) {
+//			sum += input;
+//			input = scanner.nextInt();
+//		}
+//		System.out.println(sum);
+		do {
+			input = scanner.nextInt();
+			sum += input;
+		} while ( input != 0 );
+		System.out.println(sum);
+	}
+
+}
