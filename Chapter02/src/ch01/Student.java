8a0e9d61ec64de38dfd866837d908b1156cd6020
조명환 <idmash88@gmail.com>
2023-05-30 04:12:26 +0900

    클래스 별 멤버 변수 추가

--- Chapter02/src/ch01/Student.java
+++ Chapter02/src/ch01/Student.java
@@ -1,5 +1,11 @@
 package ch01;
 
+// 학생 클래스
 public class Student {
+	
 	int studentId;
+	String studentName;
+	int majorCode;
+	String majorName;
+	int grade;
 }
---
+++ Chapter02/src/ch01/UserInfo.java
@@ -0,0 +1,11 @@
+package ch01;
+
+//회원 클래스
+public class UserInfo {
+
+	String userId;
+	String userpassword;
+	String userName;
+	String address;
+	int phoneNumber;
+}
