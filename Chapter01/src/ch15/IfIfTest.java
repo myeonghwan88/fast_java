b6be5bece7254a9636bd9bcf7512dfbf74edc342
조명환 <idmash88@gmail.com>
2023-05-30 01:08:04 +0900

    if - if 인경우

--- Chapter01/src/ch15/IfIfTest.java
+++ Chapter01/src/ch15/IfIfTest.java
@@ -13,13 +13,16 @@ public class IfIfTest {
 		if (age < 8) {
 			charge = 1000;
 			System.out.println("미 취학 아동입니다.");
-		} else if (age < 14) {
+		}
+		if (age < 14) {
 			charge = 2000;
 			System.out.println("초등학생 입니다.");
-		} else if (age < 20) {
+		} 
+		if (age < 20) {
 			charge = 2500;
 			System.out.println("중, 고등학생 입니다.");
-		} else {
+		}
+		else {
 			charge = 3000;
 			System.out.println("일반인 입니다.");
 		}
