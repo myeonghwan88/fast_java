4cc1c3aca70c9cfb3938032e66a8d88c9b034026
조명환 <idmash88@gmail.com>
2023-05-30 12:40:12 +0900

    연습

--- Chapter02/src/ch04/MyStudentTest.java
+++ Chapter02/src/ch04/MyStudentTest.java
@@ -1,20 +1,22 @@
 package ch04;
 
-public class StudentTest {
+public class MyStudentTest {
 
 	public static void main(String[] args) {
 		
-		Student studentLee = new Student(); // studentLee : 인스턴스 
+		MyStudent studentLee = new MyStudent(); // studentLee : 인스턴스 
 		
 		studentLee.studentID = 12345;
-		studentLee.setStudendName("Lee");
+		studentLee.setStudentName("Lee");
 		studentLee.address = "서울 강남구";
 		
 		studentLee.showStudentInfo();
 		
-		Student studentKim = new Student(); // studentKim : 인스턴스 
+		MyStudent studentKim = new MyStudent(); // studentKim : 인스턴스 
 		studentKim.studentID = 54321;
-		studentKim.studendName = "Kim";
+		studentKim.studentName = "Kim";
+		String test = studentKim.getStudentName();
+		System.out.println(test);
 		studentKim.address = "경기도 성남시";
 		
 		studentKim.showStudentInfo();
