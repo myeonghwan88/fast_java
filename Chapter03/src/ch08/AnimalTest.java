11127651d09a8b3f182235f5ab885449a370cfaa
조명환 <idmash88@gmail.com>
2023-06-07 01:24:51 +0900

    다운 캐스팅

--- Chapter03/src/ch08/AnimalTest.java
+++ Chapter03/src/ch08/AnimalTest.java
@@ -53,7 +53,7 @@ public class AnimalTest {
 		Animal tAnimal = new Tiger();
 		Animal eAnimal = new Eagle();
 		
-//		AnimalTest test = new AnimalTest();
+		AnimalTest test = new AnimalTest();
 //		test.moveAnimal(hAnimal);
 //		test.moveAnimal(tAnimal);
 //		test.moveAnimal(eAnimal);
@@ -70,9 +70,28 @@ public class AnimalTest {
 //		for(Animal animal : animalList) {
 //			animal.move();
 //		}
-
+		
+		test.testDownCasting(animalList);
 	}
-	
+	public void testDownCasting(ArrayList<Animal> list) {
+		
+		for (int i=0; i<list.size(); i++) {
+			Animal animal = list.get(i);
+			
+			if(animal instanceof Human) {
+				Human human = (Human)animal;
+				human.readBooks();
+			} else if(animal instanceof Tiger) {
+				Tiger tiger = (Tiger)animal;
+				tiger.hunting();
+			} else if(animal instanceof Eagle) {
+				Eagle eagle = (Eagle)animal;
+				eagle.flying();
+			} else {
+				System.out.println("error");
+			}
+		}
+	}
 	public void moveAnimal(Animal animal) {
 		animal.move();
 	}
