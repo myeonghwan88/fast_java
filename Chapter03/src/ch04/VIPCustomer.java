2be05965cdd0d923c6e79cca746f99356e2c723e
조명환 <idmash88@gmail.com>
2023-06-07 00:05:39 +0900

    오버라이딩

---
+++ Chapter03/src/ch04/VIPCustomer.java
@@ -0,0 +1,37 @@
+package ch04;
+
+public class VIPCustomer extends Customer{
+
+	double salesRatio;
+	private String agentID;
+	
+//	public VIPCustomer() {	
+//		
+//		super(0,"no-name");
+//		
+//		bonusRatio = 0.05;
+//		salesRatio = 0.1;
+//		customerGrade = "VIP";
+//		
+//		System.out.println("VIPCustomer() 생성자 호출");
+//	}
+	
+	public VIPCustomer(int customerID, String customerName) {
+		super(customerID, customerName);
+		bonusRatio = 0.05;
+		salesRatio = 0.1;
+		customerGrade = "VIP";
+	}
+	
+	@Override
+	public int calcPrice(int price) {
+		bonusPoint += price * bonusRatio;
+		price -= (int)(price * salesRatio);
+		return price;
+	}
+
+	public String getAgentID() {
+		return agentID;
+	}
+	
+}
